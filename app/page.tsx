882da83992f8a9291f42711be3d78c50d4f92c2d
shennawy <ahmedshennawy997@gmail.com>
2025-08-20 02:19:37 +0300

    cloudFlare

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useEffect } from "react";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebookF, FaTiktok } from "react-icons/fa";
